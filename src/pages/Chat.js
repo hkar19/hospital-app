e084c7b1b7c111e144bddb5ec93879c60d045951
hkar19 <hadikurniawanar@gmail.com>
2020-06-28 11:49:33 +0700

    another version of chat

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Row from "react-bootstrap/Row";
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Image from "react-bootstrap/Image";
@@ -27,58 +28,11 @@ function Chat(props) {
     name: props.user,
     // logo: props.user ? require(`../assets/avatar/${props.user}.svg`) : ""
   }
-  
-  // const populateSOAP= (data)=>{
-  //   let list = [];
-  //   list.push(
-  //     <Form className="mr-2">
-  //       <Form.Group  controlId="Subjective">
-  //         <Form.Label className="h3">S</Form.Label>
-  //         <Form.Control 
-  //           as="textarea"
-  //           // style={{backgroundImage: `url(../assets/s.png)`}}
-  //           placeholder="masukkan subjective"
-  //           value={data.S}></Form.Control>
-  //       </Form.Group>
-  //       <Form.Group  controlId="Objective">
-  //         <Form.Label className="h3">O</Form.Label>
-  //         <Form.Control 
-  //           as="textarea"
-            
-  //           placeholder="masukkan Objective"
-  //           value={data.O}></Form.Control>
-  //       </Form.Group>
-  //       <Form.Group  controlId="Analysis">
-  //         <Form.Label className="h3">A</Form.Label>
-  //         <Form.Control
-  //           as="textarea" 
-            
-  //           placeholder="masukkan Analysis"
-  //           value={data.A}></Form.Control>
-  //       </Form.Group>
-  //       <Form.Group  controlId="Planning">
-  //         <Form.Label className="h3">P</Form.Label>
-  //         <Form.Control 
-  //           as="textarea"
-            
-  //           placeholder="masukkan Planning"
-  //           value={data.P}></Form.Control>
-  //       </Form.Group>
-  //       <Row>
-  //         <ButtonGroup>
-  //           <Button variant="success" as={Col} className="mr-1 ml-1">Check patient history</Button>
-  //           <Button variant="success" as={Col} className="mr-1 ml-1">Submit SOAP</Button>
-  //         </ButtonGroup>
-  //       </Row>
-  //     </Form>
-  //   )
-  //   return list;
-  // }
 
   const populateSOAP= (data)=>{
     return (
       <Form>
-        <Card className="mb-3"> 
+        <Card className="mt-3 mb-3"> 
           <Row className="no-gutters">
             <Col md={4}>
               <Image
@@ -158,12 +112,12 @@ function Chat(props) {
             </Col>
           </Row>
         </Card>
-       <Row>
-         <ButtonGroup>
+       <Card.Footer>
+         <ButtonGroup >
            <Button variant="success" as={Col} className="mr-1 ml-1">Check patient history</Button>
            <Button variant="success" as={Col} className="mr-1 ml-1">Submit SOAP</Button>
          </ButtonGroup>
-        </Row>
+        </Card.Footer>
       </Form>
     );
   }
@@ -173,9 +127,9 @@ function Chat(props) {
       <Container>
         <Row>
           {user.name === "dokter" ? (
-              <Col xs={6} sm={5} md={4} lg={3}>
+              <Card as={Col} xs={6} sm={5} md={4} lg={3}>
                 {populateSOAP(SOAPDummy)}
-              </Col>
+              </Card>
           ): []}
             <Card as={Col}>
               
@@ -196,8 +150,7 @@ function Chat(props) {
                 >
                 </Card.Img>
               </Card.Body>
-              <Card.Footer className="align-items-end">
-                <Row className="ml-1 mr-1 justify-content-around">
+              <Card.Footer as={Row} className="justify-content-around">
                   <ButtonGroup className="">
                     <Button variant="success" as={Col} className="ml-1 mr-1">
                       <Mute className="h4"/>
@@ -212,7 +165,6 @@ function Chat(props) {
                       <EndCall className="h4"/>
                     </Button>
                   </ButtonGroup>
-                </Row>
               </Card.Footer>
             </Card>
         </Row>
