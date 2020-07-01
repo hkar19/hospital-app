8f990bb34425d016611aa9be9d27b630cbdecd2e
hkar19 <hadikurniawanar@gmail.com>
2020-07-01 10:19:13 +0700

    -changed hospital logo -modify login page (hadi ver) -added login css but not implemented yet

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -14,6 +14,8 @@ import logo from "../assets/hosp/res/mipmap-xxxhdpi/hosp.png";
 import Container from 'react-bootstrap/Container';
 import { useHistory } from 'react-router-dom';
 
+// import "./sinapsis-login.css"
+
 
 const Login = (props)=>{
   const [user,setUser]= useState("");
@@ -45,87 +47,68 @@ const Login = (props)=>{
   }
 
   return(
-  <Container>
-    <Jumbotron 
+    // <Container
+    // style={{
+    //   backgroundImage:`url(${require("../assets/doctor_background.jpg")})`,
+    //   backgroundRepeat:"no-repeat",
+    //   backgroundSize:"contain"
+    // }}
+    // >
+    //   <Jumbotron>
+
+    //   </Jumbotron>
+    // </Container>
+
+  <Container
       style={{
-        flexDirection:"column",
-        justifyContent:"center",
-        textAlign:"center",
-        margin:"3% 3%"
-        }}>
-      <Image
-        alt=""
-        src={logo}
-      ></Image>
-      <p style={{fontSize:"xx-large"}}>RS Tjahaja Hati</p>
-      <p style={{fontSize:"small"}}>powered by Sinapsis</p>
-      <Form onSubmit={handleSubmit} inline style={{flexDirection:"column"}}>
-        <Form.Group style={{justifyContent:"space-between"}}>
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="username anda"
-            value={user}
-            onChange={userChangeHandler}
-          ></Form.Control>
-        </Form.Group>
-        <Form.Group style={{justifyContent:"space-between"}}>
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="password anda"
-            value={password}
-            onChange={passwordChangeHandler}
-          ></Form.Control>
-        </Form.Group>
-        <Button type="submit">Login</Button>
-      </Form>
-    </Jumbotron>
+      backgroundImage:`url(${require("../assets/doctor_background.jpg")})`,
+      backgroundRepeat:"no-repeat",
+      // backgroundSize:"contain"
+    }}
+  >
+    <Row>
+      <Col 
+        md={"auto"}
+        style={{
+          flexDirection:"column",
+          justifyContent:"center",
+          textAlign:"center",
+          marginLeft:"auto",
+          padding:"1%"
+          }}>
+        <Image
+          alt=""
+          src={logo}
+        ></Image>
+        <p style={{fontSize:"xx-large"}}>RS Tjahaja Hati</p>
+
+        <Form onSubmit={handleSubmit} inline style={{flexDirection:"column",justifyContent:"center"}}>
+          <Form.Group style={{justifyContent:"space-between"}}>
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="username anda"
+              value={user}
+              onChange={userChangeHandler}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group style={{justifyContent:"space-between"}}>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="password anda"
+              value={password}
+              onChange={passwordChangeHandler}
+            ></Form.Control>
+          </Form.Group>
+          <Button type="submit">Login</Button>
+        </Form>
+        <p style={{fontSize:"small"}}>powered by Sinapsis</p>
+      </Col>
+    </Row>
+
   </Container>
 
-    // <Jumbotron>
-    //   <Container className="justify-content-md-center">
-    //   <Row className="align-items-center mb-2">
-    //         <Col xl={12} className="text-center">
-    //         <Image
-    //           alt=""
-    //           src={logo}
-    //           height="20%"
-    //           width="20%"
-    //         ></Image>
-    //         </Col>
-    //         <Col className="text-center">
-    //           <h2>RS Tjahaja Hati</h2>
-    //           <small>powered by Sinapsis</small>
-    //         </Col>
-    //       </Row>
-    //     <Form onSubmit={handleSubmit}>
-    //       <Form.Group as={Row} controlId="user">
-    //         <Form.Label column sm={2}>User</Form.Label>
-    //         <Col sm={10}>
-    //           <FormControl 
-    //             type="text" 
-    //             placeholder="user anda"
-    //             value={user}
-    //             onChange={userChangeHandler}  
-    //           ></FormControl>
-    //         </Col>
-    //       </Form.Group>
-    //       <Form.Group as={Row} controlId="password">
-    //         <Form.Label column sm={2}>Password</Form.Label>
-    //         <Col sm={10}>
-    //           <FormControl 
-    //             type="password" 
-    //             placeholder="masukkan password anda"
-    //             value={password}
-    //             onChange={passwordChangeHandler}></FormControl>
-    //         </Col>
-
-    //       </Form.Group>
-    //       <Button type="submit">Login</Button>
-    //     </Form>
-    //   </Container>
-    // </Jumbotron>
   );
 }
 
